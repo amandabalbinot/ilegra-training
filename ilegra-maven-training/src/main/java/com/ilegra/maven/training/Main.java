cd47d48e590068bb6dc0f45a8b34bf7d6ee47e22
Jackson <cyber.jso@gmail.colm>
2015-06-08 09:25:08 -0300

    configuring logger

--- ilegra-maven-training/src/main/java/com/ilegra/maven/training/Main.java
+++ ilegra-maven-training/src/main/java/com/ilegra/maven/training/Main.java
@@ -1,9 +1,13 @@
 package com.ilegra.maven.training;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 public class Main {
+	private final static Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        System.out.print("It Worked");
+    	logger.info("It worked!");
     }
 
  }
