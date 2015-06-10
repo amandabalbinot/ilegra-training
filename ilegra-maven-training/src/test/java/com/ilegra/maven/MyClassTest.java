ebe414c8f6050dff8763c8430957c827f44db523
Jackson <cyber.jso@gmail.colm>
2015-06-09 21:48:15 -0300

    Adding test class

---
+++ ilegra-maven-training/src/test/java/com/ilegra/maven/MyClassTest.java
@@ -0,0 +1,16 @@
+package com.ilegra.maven;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyClassTest {
+
+    @Test
+    public void firstTest() {
+        Assert.assertEquals(1, 1);
+    }
+
+    public void secondTest() {
+        Assert.assertEquals(2,2);
+    }
+}
